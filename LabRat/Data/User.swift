7c8f3fe3a2f2eb0fc22613e7b280215db6fcb163
Euan Mendoza <bebopbamf@effectfree.dev>
2024-03-27 22:52:58 +1100

    chore: added dependancies and setup initial structure

---
+++ LabRat/Data/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  LabRat
+//
+//  Created by Euan Mendoza on 27/3/2024.
+//
+
+import Foundation
+
+
+struct User: Decodable {
+    let name: String;
+}
---
+++ LabRat/Pages/Auth/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  LabRat
+//
+//  Created by Euan Mendoza on 27/3/2024.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
